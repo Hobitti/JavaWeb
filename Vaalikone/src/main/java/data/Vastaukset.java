f114a259d87f5b76dca69ec8b15a0caeec2b3460
Nyyri <75020126+Nyyri@users.noreply.github.com>
2022-04-11 18:47:37 +0300

    Lisätty Vastaukset

---
+++ Vaalikone/src/main/java/data/Vastaukset.java
@@ -0,0 +1,63 @@
+package data;
+
+public class Vastaukset {
+	private int id;
+	private int ehdokasId;
+	private int kysymysId;
+	private int vastasi;
+	private String perustelu;
+	private float average;
+	
+	public Vastaukset() {
+		//todo
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getEhdokasId() {
+		return ehdokasId;
+	}
+	
+	public void setEhdokasId(int ehdokasId) {
+		this.ehdokasId = ehdokasId;
+	}
+	
+	public int getKysymysId() {
+		return kysymysId;
+	}
+	
+	public void setKysymysId(int kysymysId) {
+		this.kysymysId = kysymysId;
+	}
+	
+	public int getVastasi() {
+		return vastasi;
+	}
+	
+	public void setVastasi(int vastasi) {
+		this.vastasi = vastasi;
+	}
+	
+	public String getPerustelu() {
+		return perustelu;
+	}
+	
+	public void setPerustelu(String perustelu) {
+		this.perustelu = perustelu;
+	}
+	
+	public float getAverage() {
+		return average;
+	}
+	
+	public void setAverage(float average) {
+		this.average = average;
+	}
+	
+}
